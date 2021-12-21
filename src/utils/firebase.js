61f11e0b92af7640b493ee0416c58cdb4262d6ca
Suman Halder <49319652+programmersumanhalder@users.noreply.github.com>
2021-12-21 20:10:45 +0200

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqwQmOb3xqGb7UgFDBCKPQXCeJf4aV2Xc",
-  authDomain: "clone-61f81.firebaseapp.com",
-  projectId: "clone-61f81",
-  storageBucket: "clone-61f81.appspot.com",
-  messagingSenderId: "186862991618",
-  appId: "1:186862991618:web:fef3dd7a6d19e66ca7ea1b",
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "FIREBASE_AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGEING_SENDER_ID",
+  appId: "APP_ID",
 };
 
 // Initialize Firebase
